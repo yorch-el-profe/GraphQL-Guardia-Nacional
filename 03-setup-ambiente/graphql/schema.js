396356d19af291a9af49b60aa45d040869aa3330
Jorge Ramón <jorge.ramon@globant.com>
2022-11-24 15:01:49 -0600

    Corrección al schema

--- 03-setup-ambiente/graphql/schema.js
+++ 03-setup-ambiente/graphql/schema.js
@@ -2,16 +2,16 @@ const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
     type Entity {
-        _id: ID
-        name: String
-        population: Int
-        latitude: Float
-        longitude: Float
+        _id: ID!
+        name: String!
+        population: Int!
+        latitude: Float!
+        longitude: Float!
     }
 
     type Query {
         getAll: [Entity]
         getById(id: ID!): Entity
-        getByName(name: String!): Entity
+        getByName(name: String!): [Entity]
     }
 `);
