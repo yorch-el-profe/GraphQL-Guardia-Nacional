05a3cb959d21a6efd48a9ed72d762599a64129ff
Jorge Ramón <jorge.ramon@globant.com>
2022-11-25 15:09:52 -0600

    Actualizando ejemplo de Apollo con AXIOS

--- 04-apollo-server/index.js
+++ 04-apollo-server/index.js
@@ -1,16 +1,51 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const axios = require('axios');
+const { GraphQLError } = require('graphql');
 
 const typeDefs = `
+    type SimplePokemon {
+        number: Int
+        name: String
+    }
+
+    type Pokemon {
+        number: Int
+        name: String
+        types: [String]
+        artwork: String
+        sprite: String
+    }
+
     type Query {
-        helloWorld: String
+        getAll: [SimplePokemon]
+        getByNumber(number: Int!): Pokemon
     }
 `;
 
 const resolvers = {
     Query: {
-        helloWorld() {
-            return "Hello World";
+        async getAll() {
+            try {
+                const response = await axios
+                    .get('https://bedu-pokemon-api.herokuapp.com/pokemon');
+
+                const { data } = response.data;
+                return data;
+            } catch (e) {
+                throw new GraphQLError('API de Pokemon no disponible');
+            }
+        },
+        async getByNumber(_, { number }) {
+            try {
+                const response = await axios
+                    .get('https://bedu-pokemon-api.herokuapp.com/pokemon/' + number);
+
+                const { data } = response.data;
+                return data;
+            } catch (e) {
+                throw new GraphQLError('API de Pokemon no disponible');
+            }
         }
     }
 }
