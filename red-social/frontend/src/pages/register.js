2d3be553e35a90d0fc94cd756cd9046bd4feb82b
Jorge Ramón <jorge.ramon@globant.com>
2022-12-14 17:32:05 -0600

    Avance de implementacion de la red social

--- red-social/frontend/src/pages/register.js
+++ red-social/frontend/src/pages/register.js
@@ -1,24 +1,51 @@
+import { useForm } from "react-hook-form";
+
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function registerHandler(data) {
+    console.log(data);
+  }
+
   return (
-    <form noValidate>
+    <form onSubmit={handleSubmit(registerHandler)} noValidate>
       <h2 className="mb-3">Registrarse</h2>
       <div className="mb-3">
-        <label for="emailInput" className="form-label">
+        <label htmlFor="emailInput" className="form-label">
           Correo Electrónico
         </label>
-        <input type="email" className="form-control" id="emailInput" />
+        <input
+          type="email"
+          className={`form-control ${errors.email ? "is-invalid" : ""}`}
+          id="emailInput"
+          {...register("email", { required: true })}
+        />
       </div>
       <div className="mb-3">
-        <label for="usernameInput" className="form-label">
+        <label htmlFor="usernameInput" className="form-label">
           Nombre de usuario
         </label>
-        <input type="text" className="form-control" id="usernameInput" />
+        <input
+          type="text"
+          className={`form-control ${errors.username ? "is-invalid" : ""}`}
+          id="usernameInput"
+          {...register("username", { required: true })}
+        />
       </div>
       <div className="mb-3">
-        <label for="passwordInput" className="form-label">
+        <label htmlFor="passwordInput" className="form-label">
           Contraseña
         </label>
-        <input type="password" className="form-control" id="passwordInput" />
+        <input
+          type="password"
+          className={`form-control ${errors.password ? "is-invalid" : ""}`}
+          id="passwordInput"
+          {...register("password", { required: true })}
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Enviar
